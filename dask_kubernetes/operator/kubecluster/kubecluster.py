6546364b41c987788c16c2c81db266ecf7c3b482
Bas Welsh <sebastian.welsh@sedarotech.com>
2022-10-30 20:51:38 -0400

    Test

--- dask_kubernetes/operator/kubecluster/kubecluster.py
+++ dask_kubernetes/operator/kubecluster/kubecluster.py
@@ -777,7 +777,7 @@ def make_worker_spec(
     if isinstance(worker_command, str):
         worker_command = worker_command.split(" ")
 
-    args = worker_command + ["--name", "$(DASK_WORKER_NAME)"]
+    args = worker_command + ["--name", "$(DASK_WORKER_NAME)", "--no-dashboard"]
 
     return {
         "replicas": n_workers,
@@ -820,7 +820,8 @@ def make_scheduler_spec(
                 {
                     "name": "scheduler",
                     "image": image,
-                    "args": ["dask-scheduler", "--host", "0.0.0.0"],
+                    # "args": ["dask-scheduler", "--host", "0.0.0.0"] + (args or []), --no-dashboard --no-jupyter --no-show
+                    "args": ["dask-scheduler", "--host", "0.0.0.0", "--no-dashboard", "--no-jupyter", "--no-show"],
                     "env": env,
                     "resources": resources,
                     "ports": [
